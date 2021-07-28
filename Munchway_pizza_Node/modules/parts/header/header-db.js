d26f4f83602101f4be449d9afdc1dc4fa9c6a98c
ghazal <ghazal.mallak@gmail.com>
2021-07-28 22:52:52 +0300

    editing

--- Munchway_pizza_Node/modules/parts/header/header-db.js
+++ Munchway_pizza_Node/modules/parts/header/header-db.js
@@ -2,13 +2,13 @@ const HeaderJson = require('./navbar/navbar.json')
 const DB = require('../../../general-db');
 
 
-let MenuItems =async () => {
+let MenuItems =async (req) => {
   return await DB.query("SELECT *FROM menulist INNER JOIN menuitems ON menulist.id=menuitems.menu_id AND menulist.id = 1");
   }
 
   
 
 
-module.exports.HeaderJson=async () => {
+module.exports.HeaderJson=async (req) => {
     return JSON.parse('{"MenuItems":'+JSON.stringify(await MenuItems())+"}")
 }
